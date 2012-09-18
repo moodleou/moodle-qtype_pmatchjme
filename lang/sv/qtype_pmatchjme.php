d7b4f603b15bf08d180851816feaa94406e75165
Patrik Granlöv <patrik.granlov@lnu.se>
2012-09-18 11:58:34 +0200

    Swedish translation
    sv lang file

---
+++ lang/sv/qtype_pmatchjme.php
@@ -0,0 +1,44 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    qtype
+ * @subpackage pmatchjme
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['allowanothertry'] = 'Tillåt ett nytt försök';
+$string['enablejava'] = 'Det gick inte att ladda JME-redigeraren. Du måste ha Java Runtime Environment (JRE) till din webbläsare för att kunna genomföra den här frågan.';
+$string['enablejavaandjavascript'] = 'Laddar JME-redigeraren.... Om inte detta meddelande ersätts med JME-redigeraren beror det nog förmodligen på att javascript och Java Runtime Environ (JRE) inte fungerar i din webbläsare.';
+$string['pluginname'] = 'Mönster med JME-editor';
+$string['smiles_Br'] = 'bromatomer';
+$string['smiles_c'] = 'aromatiska kolatomer';
+$string['smiles_C'] = 'alifatiska kolatomer';
+$string['smiles_Cl'] = 'kloratomer';
+$string['smiles_equals'] = 'dubbelbindningar';
+$string['smiles_F'] = 'fluoratomer';
+$string['smiles_hash'] = 'trippelbindningar';
+$string['smiles_I'] = 'jodatomer';
+$string['smiles_N'] = 'kväveatomer';
+$string['smiles_O'] = 'syreatomer';
+$string['smiles_S'] = 'svavelatomer';
+$string['smilescorrectcount'] = 'Du har rätt molekylformel, men fel struktur.';
+$string['smilesequal'] = 'Du har rätt antal {$a}.';
+$string['smilestoofew'] = 'Du har för få {$a}.';
+$string['smilestoomany'] = 'Du har för många {$a}.';

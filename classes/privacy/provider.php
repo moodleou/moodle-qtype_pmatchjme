moodleou/moodle-qtype_oumultiresponse

Questions: Default options and privacy declaration for OU qtypes #467835

classes/privacy/provider.php
@@ -21,21 +21,77 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 namespace qtype_oumultiresponse\privacy;
+
+use \core_privacy\local\metadata\collection;
+use \core_privacy\local\request\transform;
+use \core_privacy\local\request\user_preference_provider;
+use \core_privacy\local\request\writer;
+
 defined('MOODLE_INTERNAL') || die();
 /**
- * Privacy Subsystem for qtype_oumultiresponse implementing null_provider.
+ * Privacy Subsystem for qtype_oumultiresponse implementing user_preference_provider.
  *
  * @copyright  2018 The Open University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements
+        // This component has data.
+        // We need to return default options that have been set a user preferences.
+        \core_privacy\local\metadata\provider,
+        \core_privacy\local\request\user_preference_provider {
+
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Returns meta data about this system.
      *
-     * @return  string
+     * @param collection $collection The initialised collection to add items to.
+     * @return  collection     A listing of user data stored through this system.
      */
-    public static function get_reason() : string {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_user_preference('qtype_oumultiresponse_defaultmark', 'privacy:preference:defaultmark');
+        $collection->add_user_preference('qtype_oumultiresponse_penalty', 'privacy:preference:penalty');
+        $collection->add_user_preference('qtype_oumultiresponse_shuffleanswers', 'privacy:preference:shuffleanswers');
+        $collection->add_user_preference('qtype_oumultiresponse_answernumbering', 'privacy:preference:answernumbering');
+        $collection->add_user_preference('qtype_oumultiresponse_showstandardinstruction',
+                'privacy:preference:showstandardinstruction');
+        return $collection;
+    }
+
+    /**
+     * Export all user preferences for the plugin.
+     *
+     * @param int $userid The userid of the user whose data is to be exported.
+     */
+    public static function export_user_preferences(int $userid) {
+        $preference = get_user_preferences('qtype_oumultiresponse_defaultmark', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:defaultmark', 'qtype_oumultiresponse');
+            writer::export_user_preference('qtype_oumultiresponse', 'defaultmark', $preference, $desc);
+        }
+
+        $preference = get_user_preferences('qtype_oumultiresponse_penalty', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:penalty', 'qtype_oumultiresponse');
+            writer::export_user_preference('qtype_oumultiresponse', 'penalty', transform::percentage($preference), $desc);
+        }
+
+        $preference = get_user_preferences('qtype_oumultiresponse_answernumbering', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:answernumbering', 'qtype_oumultiresponse');
+            writer::export_user_preference('qtype_oumultiresponse', 'answernumbering',
+                    get_string('answernumbering' . $preference, 'qtype_multichoice'), $desc);
+        }
+
+        $preference = get_user_preferences('qtype_oumultiresponse_shuffleanswers', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:shuffleanswers', 'qtype_multichoice');
+            writer::export_user_preference('qtype_oumultiresponse', 'shuffleanswers', transform::yesno($preference), $desc);
+        }
+
+        $preference = get_user_preferences("qtype_oumultiresponse_showstandardinstruction", null, $userid);
+        if (null !== $preference) {
+            $desc = get_string("privacy:preference:showstandardinstruction", 'qtype_oumultiresponse');
+            writer::export_user_preference('qtype_oumultiresponse', 'showstandardinstruction',
+                    transform::yesno($preference), $desc);
+        }
     }
 }

moodleou/moodle-qtype_pmatchjme

classes/privacy/provider.php
@@ -14,28 +14,72 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Privacy Subsystem implementation for qtype_pmatchjme.
+ * Privacy Subsystem implementation for qtype_.pmatchjme.
  *
  * @package    qtype_pmatchjme
  * @copyright  2018 The Open University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 namespace qtype_pmatchjme\privacy;
+
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\writer;
+use core_privacy\local\request\transform;
+
 defined('MOODLE_INTERNAL') || die();
 /**
- * Privacy Subsystem for qtype_pmatchjme implementing null_provider.
+ * Privacy Subsystem for qtype_pmatchjme implementing user_preference_provider.
  *
  * @copyright  2018 The Open University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements
+        // This component has data.
+        // We need to return default options that have been set a user preferences.
+        \core_privacy\local\metadata\provider,
+        \core_privacy\local\request\user_preference_provider {
+
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Returns meta data about this system.
      *
-     * @return  string
+     * @param collection $collection The initialised collection to add items to.
+     * @return  collection     A listing of user data stored through this system.
      */
-    public static function get_reason() : string {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_user_preference('qtype_pmatchjme_defaultmark', 'privacy:preference:defaultmark');
+        $collection->add_user_preference('qtype_pmatchjme_penalty', 'privacy:preference:penalty');
+        $collection->add_user_preference('qtype_pmatchjme_allowsuperscript', 'privacy:preference:allowsuperscript');
+        $collection->add_user_preference('qtype_pmatchjme_allowsubscript', 'privacy:preference:allowsubscript');
+        return $collection;
+    }
+
+    /**
+     * Export all user preferences for the plugin.
+     *
+     * @param int $userid The userid of the user whose data is to be exported.
+     */
+    public static function export_user_preferences(int $userid) {
+        $preference = get_user_preferences('qtype_pmatchjme_defaultmark', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:defaultmark', 'qtype_pmatchjme');
+            writer::export_user_preference('qtype_pmatchjme', 'defaultmark', $preference, $desc);
+        }
+
+        $preference = get_user_preferences('qtype_pmatchjme_penalty', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:penalty', 'qtype_pmatchjme');
+            writer::export_user_preference('qtype_pmatchjme', 'penalty', transform::percentage($preference), $desc);
+        }
+        $preferences = [
+                'allowsuperscript',
+                'allowsubscript'
+        ];
+        foreach ($preferences as $key) {
+            $preference = get_user_preferences("qtype_pmatchjme_{$key}", null, $userid);
+            if (null !== $preference) {
+                $desc = get_string("privacy:preference:{$key}", 'qtype_pmatchjme');
+                writer::export_user_preference('qtype_pmatchjme', $key, transform::yesno($preference), $desc);
+            }
+        }
     }
 }